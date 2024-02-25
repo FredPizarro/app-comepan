7f53e51faba59a4b06cfd051b233c10082d376ae
Jhostin <shoockwaveeyt@gmail.com>
2024-02-25 15:08:36 -0500

    panel-login

--- src/app/componentes/nav/nav.component.ts
+++ src/app/componentes/nav/nav.component.ts
@@ -6,7 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.scss'],
 })
 export class NavComponent  implements OnInit {
+  mostrarLogin: boolean = false;
 
+  toggleLoginPanel() {
+    this.mostrarLogin = !this.mostrarLogin;
+  }
+  // Método para cerrar sesión
+  cerrarSesion() {
+    // Aquí puedes realizar las acciones necesarias para cerrar la sesión del usuario,
+    // como limpiar el estado de autenticación, redirigir a la página de inicio, etc.
+    // Por ejemplo:
+    // Limpiar datos de sesión o token de autenticación
+    // Redirigir a la página de inicio
+    this.mostrarLogin = false; // Ocultar el panel de inicio de sesión después de cerrar sesión
+  }
+  
   constructor() { }
 
   ngOnInit() {}
